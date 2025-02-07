a42b81973ddf2518b6b057458a5bace82595ee73
Aetopia <41850963+Aetopia@users.noreply.github.com>
2025-02-07 23:04:44 +0530

    Remove unused constant.

--- src/Unmanaged/Constants.cs
+++ src/Unmanaged/Constants.cs
@@ -6,7 +6,5 @@ static class Constants
 
     internal const int SYNCHRONIZE = unchecked((int)0x00100000L);
 
-    internal const int WAIT_OBJECT_0 = unchecked((int)0x00000000L);
-
     internal const int CLSCTX_INPROC_SERVER = 0x1;
 }
