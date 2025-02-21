31a7172f4c01f47e58d7bd4af0ebda4baadbc9c9
Aetopia <41850963+Aetopia@users.noreply.github.com>
2025-02-21 14:20:21 +0530

    Cleanup code for supporting .NET Framework.

--- src/Unmanaged/Native/Core.cs
+++ src/Unmanaged/Native/Core.cs
@@ -1,3 +1,4 @@
+#if NET
 using System;
 using System.Runtime.InteropServices;
 
@@ -7,7 +8,6 @@ namespace Bedrockix.Unmanaged;
 
 static partial class Native
 {
-#if NET
     [LibraryImport("Kernel32", SetLastError = true)]
     internal static partial nint OpenProcess(int dwDesiredAccess, [MarshalAs(UnmanagedType.Bool)] bool bInheritHandle, int dwProcessId);
 
@@ -47,45 +47,5 @@ static partial class Native
 
     [LibraryImport("Kernel32", SetLastError = true)]
     internal static partial int GetPackagesByPackageFamily([MarshalAs(UnmanagedType.LPWStr)] string packageFamilyName, out uint count, nint packageFullNames, out uint bufferLength, nint buffer);
-#elif NETFRAMEWORK
-    [DllImport("Kernel32", SetLastError = true)]
-    internal static extern nint OpenProcess(int dwDesiredAccess, [MarshalAs(UnmanagedType.Bool)] bool bInheritHandle, int dwProcessId);
-
-    [DllImport("Kernel32", SetLastError = true, EntryPoint = "LoadLibraryExW")]
-    internal static extern nint LoadLibraryEx([MarshalAs(UnmanagedType.LPWStr)] string lpLibFileName, nint hFile, int dwFlags);
-
-    [DllImport("Kernel32", SetLastError = true)]
-    [return: MarshalAs(UnmanagedType.Bool)]
-    internal static extern bool FreeLibrary(nint hLibModule);
-
-    [DllImport("Kernel32", SetLastError = true)]
-    internal static extern nint CreateRemoteThread(nint hProcess, nint lpThreadAttributes, int dwStackSize, nint lpStartAddress, nint lpParameter, int dwCreationFlags, nint lpThreadId);
-
-    [DllImport("Kernel32", SetLastError = true)]
-    internal static extern nint GetProcAddress(nint hModule, [MarshalAs(UnmanagedType.LPStr)] string lpProcName);
-
-    [DllImport("Kernel32", SetLastError = true)]
-    [return: MarshalAs(UnmanagedType.Bool)]
-    internal static extern bool VirtualFreeEx(nint hProcess, nint lpAddress, int dwSize, int dwFreeType);
-
-    [DllImport("Kernel32", SetLastError = true)]
-    internal static extern nint VirtualAllocEx(nint hProcess, nint lpAddress, int dwSize, int flAllocationType, int flProtect);
-
-    [DllImport("Kernel32", SetLastError = true)]
-    [return: MarshalAs(UnmanagedType.Bool)]
-    internal static extern bool WriteProcessMemory(nint hProcess, nint lpBaseAddress, nint lpBuffer, int nSize, nint lpNumberOfBytesWritten);
-
-    [DllImport("Kernel32", SetLastError = true)]
-    [return: MarshalAs(UnmanagedType.Bool)]
-    internal static extern bool CloseHandle(nint hObject);
-
-    [DllImport("Kernel32", SetLastError = true)]
-    internal static extern int WaitForMultipleObjects(int nCount, [MarshalAs(UnmanagedType.LPArray)] nint[] lpHandles, [MarshalAs(UnmanagedType.Bool)] bool bWaitAll, int dwMilliseconds);
-
-    [DllImport("Kernel32", SetLastError = true)]
-    internal static extern int WaitForSingleObject(nint hHandle, int dwMilliseconds);
-
-    [DllImport("Kernel32", SetLastError = true)]
-    internal static extern int GetPackagesByPackageFamily([MarshalAs(UnmanagedType.LPWStr)] string packageFamilyName, out uint count, nint packageFullNames, out uint bufferLength, nint buffer);
-#endif
 }
+#endif
---
+++ src/Unmanaged/Native/Framework.cs
@@ -0,0 +1,50 @@
+#if NETFRAMEWORK
+using System.Runtime.InteropServices;
+
+[assembly: DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
+
+namespace Bedrockix.Unmanaged;
+
+static partial class Native
+{
+    [DllImport("Kernel32", SetLastError = true)]
+    internal static extern nint OpenProcess(int dwDesiredAccess, [MarshalAs(UnmanagedType.Bool)] bool bInheritHandle, int dwProcessId);
+
+    [DllImport("Kernel32", SetLastError = true, EntryPoint = "LoadLibraryExW")]
+    internal static extern nint LoadLibraryEx([MarshalAs(UnmanagedType.LPWStr)] string lpLibFileName, nint hFile, int dwFlags);
+
+    [DllImport("Kernel32", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    internal static extern bool FreeLibrary(nint hLibModule);
+
+    [DllImport("Kernel32", SetLastError = true)]
+    internal static extern nint CreateRemoteThread(nint hProcess, nint lpThreadAttributes, int dwStackSize, nint lpStartAddress, nint lpParameter, int dwCreationFlags, nint lpThreadId);
+
+    [DllImport("Kernel32", SetLastError = true)]
+    internal static extern nint GetProcAddress(nint hModule, [MarshalAs(UnmanagedType.LPStr)] string lpProcName);
+
+    [DllImport("Kernel32", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    internal static extern bool VirtualFreeEx(nint hProcess, nint lpAddress, int dwSize, int dwFreeType);
+
+    [DllImport("Kernel32", SetLastError = true)]
+    internal static extern nint VirtualAllocEx(nint hProcess, nint lpAddress, int dwSize, int flAllocationType, int flProtect);
+
+    [DllImport("Kernel32", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    internal static extern bool WriteProcessMemory(nint hProcess, nint lpBaseAddress, nint lpBuffer, int nSize, nint lpNumberOfBytesWritten);
+
+    [DllImport("Kernel32", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    internal static extern bool CloseHandle(nint hObject);
+
+    [DllImport("Kernel32", SetLastError = true)]
+    internal static extern int WaitForMultipleObjects(int nCount, [MarshalAs(UnmanagedType.LPArray)] nint[] lpHandles, [MarshalAs(UnmanagedType.Bool)] bool bWaitAll, int dwMilliseconds);
+
+    [DllImport("Kernel32", SetLastError = true)]
+    internal static extern int WaitForSingleObject(nint hHandle, int dwMilliseconds);
+
+    [DllImport("Kernel32", SetLastError = true)]
+    internal static extern int GetPackagesByPackageFamily([MarshalAs(UnmanagedType.LPWStr)] string packageFamilyName, out uint count, nint packageFullNames, out uint bufferLength, nint buffer);
+}
+#endif
