d13c88d493c841310b51ff650ab5ef6b711ae330
Aetopia <41850963+Aetopia@users.noreply.github.com>
2025-02-21 14:30:55 +0530

    Format.

--- src/Windows/Handle.cs
+++ src/Windows/Handle.cs
@@ -13,10 +13,13 @@ readonly struct Handle : IDisposable
     public static implicit operator nint(Handle value) => value.Object;
 
     public void Dispose() => CloseHandle(Object);
+
 #if NET
-    internal static void Any(params ReadOnlySpan<nint> value) => WaitForMultipleObjects(value.Length, value, false, Timeout.Infinite);
+    internal static void Any(params ReadOnlySpan<nint> value) =>
 #elif NETFRAMEWORK
-    internal static void Any(params nint[] value) => WaitForMultipleObjects(value.Length, value, false, Timeout.Infinite);
+    internal static void Any(params nint[] value) =>
 #endif
+    WaitForMultipleObjects(value.Length, value, false, Timeout.Infinite);
+
     internal static void Single(nint value) => WaitForSingleObject(value, Timeout.Infinite);
 }
