afa81387bbed7f469291752fa364b616729543c5
Aetopia <41850963+Aetopia@users.noreply.github.com>
2025-02-28 11:57:20 +0530

    Apply attribute to entire class.

--- src/Unmanaged/Native/Framework.cs
+++ src/Unmanaged/Native/Framework.cs
@@ -1,48 +1,49 @@
-using System.Runtime.InteropServices;
 using System.Security;
+using System.Runtime.InteropServices;
 
 [assembly: DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
 
 namespace Bedrockix.Unmanaged;
 
-static partial class Native
+[SuppressUnmanagedCodeSecurity]
+static class Native
 {
-    [DllImport("Kernel32", SetLastError = true, CharSet = CharSet.Auto), SuppressUnmanagedCodeSecurity]
+    [DllImport("Kernel32", SetLastError = true, CharSet = CharSet.Auto)]
     internal static extern nint GetModuleHandle(string lpModuleName);
 
-    [DllImport("Kernel32", SetLastError = true), SuppressUnmanagedCodeSecurity]
+    [DllImport("Kernel32", SetLastError = true)]
     internal static extern nint OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
 
-    [DllImport("Kernel32", SetLastError = true, CharSet = CharSet.Auto), SuppressUnmanagedCodeSecurity]
+    [DllImport("Kernel32", SetLastError = true, CharSet = CharSet.Auto)]
     internal static extern nint CreateEvent(nint lpEventAttributes, bool bManualReset, bool bInitialState, nint lpName);
 
-    [DllImport("Kernel32", SetLastError = true), SuppressUnmanagedCodeSecurity]
+    [DllImport("Kernel32", SetLastError = true)]
     internal static extern bool SetEvent(nint hEvent);
 
-    [DllImport("Kernel32", SetLastError = true), SuppressUnmanagedCodeSecurity]
+    [DllImport("Kernel32", SetLastError = true)]
     internal static extern nint CreateRemoteThread(nint hProcess, nint lpThreadAttributes, int dwStackSize, nint lpStartAddress, nint lpParameter, int dwCreationFlags, nint lpThreadId);
 
-    [DllImport("Kernel32", SetLastError = true), SuppressUnmanagedCodeSecurity]
+    [DllImport("Kernel32", SetLastError = true)]
     internal static extern nint GetProcAddress(nint hModule, string lpProcName);
 
-    [DllImport("Kernel32", SetLastError = true), SuppressUnmanagedCodeSecurity]
+    [DllImport("Kernel32", SetLastError = true)]
     internal static extern bool VirtualFreeEx(nint hProcess, nint lpAddress, int dwSize, int dwFreeType);
 
-    [DllImport("Kernel32", SetLastError = true), SuppressUnmanagedCodeSecurity]
+    [DllImport("Kernel32", SetLastError = true)]
     internal static extern nint VirtualAllocEx(nint hProcess, nint lpAddress, int dwSize, int flAllocationType, int flProtect);
 
-    [DllImport("Kernel32", SetLastError = true, CharSet = CharSet.Auto), SuppressUnmanagedCodeSecurity]
+    [DllImport("Kernel32", SetLastError = true, CharSet = CharSet.Auto)]
     internal static extern bool WriteProcessMemory(nint hProcess, nint lpBaseAddress, string lpBuffer, int nSize, nint lpNumberOfBytesWritten);
 
-    [DllImport("Kernel32", SetLastError = true), SuppressUnmanagedCodeSecurity]
+    [DllImport("Kernel32", SetLastError = true)]
     internal static extern bool CloseHandle(nint hObject);
 
-    [DllImport("Kernel32", SetLastError = true), SuppressUnmanagedCodeSecurity]
+    [DllImport("Kernel32", SetLastError = true)]
     internal unsafe static extern int WaitForMultipleObjects(int nCount, nint* lpHandles, bool bWaitAll, int dwMilliseconds);
 
-    [DllImport("Kernel32", SetLastError = true), SuppressUnmanagedCodeSecurity]
+    [DllImport("Kernel32", SetLastError = true)]
     internal static extern int WaitForSingleObject(nint hHandle, int dwMilliseconds);
 
-    [DllImport("Kernel32", SetLastError = true, CharSet = CharSet.Auto), SuppressUnmanagedCodeSecurity]
+    [DllImport("Kernel32", SetLastError = true, CharSet = CharSet.Auto)]
     internal static extern int GetPackagesByPackageFamily(string packageFamilyName, out uint count, nint packageFullNames, out uint bufferLength, nint buffer);
 }
