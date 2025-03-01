a19341482d3ec40d1bdf689404800d75757118d4
Aetopia <41850963+Aetopia@users.noreply.github.com>
2025-03-01 08:17:57 +0530

    Format.

--- src/Unmanaged/Constants.cs
+++ src/Unmanaged/Constants.cs
@@ -4,9 +4,9 @@ static class Constants
 {
     internal const int AO_NOERRORUI = 0x00000002;
 
-    internal const int MEM_RELEASE = 0x00008000;
-
     internal const int PROCESS_ALL_ACCESS = 0X1FFFFF;
+
+    internal const int MEM_RELEASE = 0x00008000;
 
     internal const int MEM_COMMIT = 0x00001000;
 
