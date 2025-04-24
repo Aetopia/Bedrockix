0957dd19b07ecc39389c5f7bd07a1c1add2999a2
Aetopia <41850963+Aetopia@users.noreply.github.com>
2025-04-24 20:14:06 +0530

    Format.

--- src/Windows/App.cs
+++ src/Windows/App.cs
@@ -51,14 +51,7 @@ public partial class App
 
     public partial bool Unpackaged => Package.IsDevelopmentMode;
 
-    public partial bool Installed
-    {
-        get
-        {
-            GetPackagesByPackageFamily(Name, out var @this);
-            return @this;
-        }
-    }
+    public partial bool Installed { get { GetPackagesByPackageFamily(Name, out var @this); return @this; } }
 
     public partial bool Debug
     {
@@ -70,14 +63,7 @@ public partial class App
         }
     }
 
-    public partial bool Running
-    {
-        get
-        {
-            foreach (var _ in Processes) return true;
-            return false;
-        }
-    }
+    public partial bool Running { get { foreach (var _ in Processes) return true; return false; } }
 
     public partial void Terminate() => Settings.TerminateAllProcesses(Package.Id.FullName);
 }
