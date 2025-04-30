723c3f19a024155916d7f4ea8f20b9c9931eeaeb
Aetopia <41850963+Aetopia@users.noreply.github.com>
2025-04-30 22:39:53 +0530

    Rename field.

--- src/Unmanaged/Structures.cs
+++ src/Unmanaged/Structures.cs
@@ -62,9 +62,9 @@ readonly ref struct Address(nint @this, nint @params, int @object) : IDisposable
 
 readonly ref struct Thread(nint @this, nint @params, nint @object) : IDisposable
 {
-    readonly Handle Handle = new(Unsafe.CreateRemoteThread(@this, default, default, @params, @object, default, default));
+    readonly Handle _= new(Unsafe.CreateRemoteThread(@this, default, default, @params, @object, default, default));
 
-    public void Dispose() => Handle.Dispose();
+    public void Dispose() => _.Dispose();
 
-    public void Wait() => Safe.WaitForSingleObject(Handle);
+    public void Wait() => Safe.WaitForSingleObject(_);
 }
