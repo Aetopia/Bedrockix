2a8f1edcd5c49b7a77ff634137a4afacb1a0624b
Aetopia <41850963+Aetopia@users.noreply.github.com>
2025-05-01 07:30:55 +0530

    Refactor & cleanup code again!

--- src/Core/Game.cs
+++ src/Core/Game.cs
@@ -21,15 +21,25 @@ public sealed partial class Game : App
         {
             Handle? @params = default;
 
-            if (!Running || (@params = CreateFile(@this)) is not null)
+            try
             {
-                Process @object = new(base.Launch());
+                if (!Running || (@params = CreateFile(@this)).HasValue)
+                {
+                    Process @object = new(base.Launch());
 
-                do if ((@params ??= CreateFile(@this)) is not null) break; while (@object[true]);
-                using (@params) do if (GetFileInformationByHandleEx(@params)) break; while (@object[true]);
+                    do
+                        if (@params.HasValue) break;
+                        else @params = CreateFile(@this);
+                    while (@object[true]);
 
-                return @object;
+                    do
+                        if (GetFileInformationByHandleEx(@params)) break;
+                    while (@object[true]);
+
+                    return @object;
+                }
             }
+            finally { @params?.Dispose(); }
 
             return new(base.Launch());
         }
